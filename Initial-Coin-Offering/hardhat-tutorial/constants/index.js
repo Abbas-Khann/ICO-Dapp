2c255f3aab946454e662fb36a090ee05794d3542
Abbas Khan <78753275+Abbas-glitch@users.noreply.github.com>
2022-02-03 19:15:55 +0430

    Add files via upload

---
+++ Initial-Coin-Offering/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xaaFa1d3DB8e1a73a6328d3649c3f5CD8C9F86926";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
